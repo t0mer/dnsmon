867a311bac62ff462c86e3d59e27a7a155d16339
Tomer Klein <tomer.klein@gmail.com>
2023-12-16 18:56:49 +0200

    Added record related API endpoints

--- app/server.py
+++ app/server.py
@@ -16,6 +16,11 @@ class Server:
                 "name": "servers",
                 "description": "Get servers list",
             },
+            {
+                "name": "records",
+                "description": "Get list of record types",
+            },
+        
         ]
 
         self.app = FastAPI(title="DnsMon", description="DNS records propogation checker and change detection", version='1.0.0', openapi_tags=self.tags_metadata, contact={"name": "Tomer Klein", "email": "tomer.klein@gmail.com", "url": "https://github.com/t0mer/wmd-servers-scrapper"})
@@ -31,6 +36,26 @@ class Server:
             except Exception as e:
                 logger.error("Error fetch images, " + str(e))
                 return None
+
+        @self.app.get("/api/types",tags=['records'], summary="Get list of record types")
+        def get_servers(request: Request):
+            try:
+                records=['A','AAAA','CNAME','MX','NS','PTR','SOA','SRV','TXT','CAA']
+                return JSONResponse(records)
+            except Exception as e:
+                logger.error("Error fetch images, " + str(e))
+                return None
+            
+        @self.app.get("/api/monitored",tags=['records'], summary="Get list of monitored records")
+        def get_servers(request: Request):
+            try:
+                monitored_records = self.connector.get_monitored_records(True)
+                return JSONResponse(monitored_records)
+            except Exception as e:
+                logger.error("Error fetch images, " + str(e))
+                return None
+            
+           
 
     def start(self):
         uvicorn.run(self.app, host="0.0.0.0", port=8082)
