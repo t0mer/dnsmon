c0417edc4a718926d845b4da4017673b53a7f4ef
Tomer Klein <tomer.klein@gmail.com>
2023-12-22 14:40:19 +0200

    adding and chamging funcunality

--- app/server.py
+++ app/server.py
@@ -1,10 +1,21 @@
 import uvicorn
 import requests
-from loguru import logger
-from fastapi.responses import JSONResponse
-from sqliteconnector import SqliteConnector
-from fastapi import FastAPI, Request
 from utils import Utils
+from loguru import logger
+from sqliteconnector import SqliteConnector
+from fastapi import FastAPI, Request, File, Form, UploadFile
+from fastapi.responses import UJSONResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
+from starlette.responses import FileResponse
+from starlette.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
+from starlette_exporter import PrometheusMiddleware, handle_metrics
+
+
 class Server:
     def __init__(self):
         self.connector = SqliteConnector()
@@ -24,21 +35,44 @@ class Server:
         ]
 
         self.app = FastAPI(title="DnsMon", description="DNS records propogation checker and change detection", version='1.0.0', openapi_tags=self.tags_metadata, contact={"name": "Tomer Klein", "email": "tomer.klein@gmail.com", "url": "https://github.com/t0mer/wmd-servers-scrapper"})
+        self.app.mount("/dist", StaticFiles(directory="dist"), name="dist")
+        self.app.mount("/plugins", StaticFiles(directory="plugins"), name="plugins")
+        self.app.mount("/js", StaticFiles(directory="dist/js"), name="js")
+        self.app.mount("/css", StaticFiles(directory="dist/css"), name="css")
+        # self.app.mount("/preview", StaticFiles(directory="preview"), name="css")
+        self.templates = Jinja2Templates(directory="templates/")
+        self.app.add_middleware(PrometheusMiddleware)
+        self.app.add_route("/metrics", handle_metrics)
+        self.origins = ["*"]
+
+        self.app.add_middleware(
+            CORSMiddleware,
+            allow_origins=self.origins,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+
+
+        @self.app.get("/")
+        def home(request: Request):
+            # vtotal_enabled = VT_API_KEY != ""
+            logger.info("Loading default page")
+            return self.templates.TemplateResponse('index.html', context={'request': request })
+
+
+
 
         @self.app.get("/api/servers",tags=['servers'], summary="Get list of servers")
         def get_servers(request: Request):
             try:
-                servers = requests.get(self.servers_url).json()
-                for server in servers:
-                    self.connector.add_server(server['id'], server['latitude'],server['longitude'],server['location'],server['provider']
-                                              ,server['country'],server['status'])
                 return JSONResponse(self.connector.get_active_servers(True))
             except Exception as e:
                 logger.error("Error fetch images, " + str(e))
                 return None
 
         @self.app.get("/api/types",tags=['records'], summary="Get list of record types")
-        def get_servers(request: Request):
+        def get_types(request: Request):
             try:
                 return JSONResponse(self.utils.types)
             except Exception as e:
