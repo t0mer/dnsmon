26561661105c0d71dbff2e85cc534a0cdaea3dc8
Tomer Klein <tomer.klein@gmail.com>
2023-12-23 15:23:08 +0200

    adding servers table to the dashboard

--- app/dist/js/main.js
+++ app/dist/js/main.js
@@ -20,7 +20,25 @@ function get_servers() {
         $('#servers_count').text(totalServers);
         $('#countries_count').text(Object.keys(uniqueCountries).length);
 
+        $.each(data, function (i, item) {
+             var $tr = $('<tr>').append(
+                $('<td>').html("<img class='flag' src='/dist/img/flags/" +item["country"] + ".svg' />"),
+                $('<td>').text(item["latitude"]),
+                $('<td>').text(item["longitude"]),
+                $('<td>').text(item["location"]),
+                $('<td>').text(item["provider"])
+              );
+   
+            $('#wmd-servers').append('<tr>' + $tr.wrap('<tr>').html() + '</tr>');
+          });
+        //   $('#loading').hide();
 
+          $('#wmd-servers').DataTable({
+            "searching":false,
+            "showing": false,
+            "bLengthChange" : false,
+
+        });
 
     });
 }
@@ -33,3 +51,44 @@ function get_monitored_domains_count() {
 
 
 
+function getDevices() {
+    $.ajax({
+        type: "get",
+        url: "api/servers",
+        dataType: "json",
+        success: function (data) {
+            if(JSON.stringify(data).includes("error"))
+            {
+                // alert("error");
+                // $('#xia_devices').hide();
+                // $('#loading').hide();
+            }
+            else
+            {
+                $.each(data, function (i, item) {
+                    var $tr = $('<tr>').append(
+                        $('<td>').text(item["country"]),
+                        $('<td>').text(item["latitude"]),
+                        $('<td>').text(item["longitude"]),
+                        $('<td>').text(item["location"]),
+                        $('<td>').text(item["provider"])
+                      );
+           
+                    $('#wmd-servers').append('<tr>' + $tr.wrap('<tr>').html() + '</tr>');
+                  });
+                //   $('#loading').hide();
+                  $('#wmd-servers').show();
+                  $('.tokens').DataTable({
+                    dom: 'Bfrtip',
+                    buttons: [
+                        'copyHtml5',
+                        'excelHtml5',
+                        'pdfHtml5'
+                    ]
+                });
+                  
+            }
+            
+        }
+    });
+}
